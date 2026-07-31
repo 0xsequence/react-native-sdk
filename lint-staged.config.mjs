3ad17f7128d1bf990d3fd6b5df62b456a43d328f
tolgahan-arikan <tolgahan.arikan@gmail.com>
2026-07-31 15:09:35 +0300

    ci(lint): validate Node module scripts

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -3,10 +3,9 @@ function command(name, files) {
 }
 
 export default {
-  '*.{js,ts,tsx}': (files) => [
+  '*.{js,cjs,mjs,ts,tsx}': (files) => [
     command('eslint --fix', files),
     command('prettier --write', files),
   ],
-  '*.{mjs,cjs}': (files) => command('prettier --write', files),
   '*.{json,yaml,yml}': (files) => command('prettier --write', files),
 };
